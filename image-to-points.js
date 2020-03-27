0aaacaf29e5a6b4ef229959c3736a84a7a58b930
patryk-kiedrowski <patkiedr@gmail.com>
2020-03-27 19:22:09 +0100

    added controls and introduced es6 syntax

--- image-to-points.js
+++ image-to-points.js
@@ -1,35 +1,38 @@
 const wrapper = document.getElementById('wrapper');
 document.addEventListener('mousemove', handleMove);
 
-var myMouseX = 0;
-var myMouseY = 0;
-var img;
-var i = 0;
-var j = 0;
-var pixels = [];
-var points = [];
-var mouse;
-var zeroVector;
-var inverseScaleUp;
+let myMouseX = 0;
+let myMouseY = 0;
+let img;
+let i = 0;
+let j = 0;
+let pixels = [];
+let points = [];
+let mouse;
+let zeroVector;
+let inverseScaleUp;
 
-var fpsCounter = document.querySelector('#fps');
-var inputs = {
+const fpsCounter = document.querySelector('#fps');
+const inputs = {
   'scaleUp': document.querySelector('#scaleUp'),
   'brightnessThreshold': document.querySelector('#brightnessThreshold'),
+  'probability': document.querySelector('#probability'),
   'pointSize': document.querySelector('#pointSize'),
   'customFrameRate': document.querySelector('#customFrameRate'),
   'mouseDistanceForInteraction': document.querySelector('#mouseDistanceForInteraction'),
   'pointColor': document.querySelector('#pointColor'),
-  'file': document.querySelector('#file')
+  'file': document.querySelector('#file'),
+  'apply-changes': document.querySelector('#apply-changes'),
 };
 
 // settings variables
-var scaleUp = 1;
-var brightnessThreshold = 40;
-var pointSize = 1.5;
-var customFrameRate = 24;
-var mouseDistanceForInteraction = 25;
-var pointColor = { 'r': 9, 'g': 133, 'b': 175 };
+let scaleUp = 1;
+let brightnessThreshold = 40;
+let pointSize = 2.5;
+let customFrameRate = 30;
+let mouseDistanceForInteraction = 50;
+let pointColor = { 'r': 9, 'g': 133, 'b': 175 };
+let probability = 30;
 
 function handleMove(event) {
   myMouseX = Math.floor(event.clientX);
@@ -37,37 +40,46 @@ function handleMove(event) {
 }
 
 function hexToRGB(hex) {
-  var r = parseInt(hex.slice(1, 3), 16),
-      g = parseInt(hex.slice(3, 5), 16),
-      b = parseInt(hex.slice(5, 7), 16);
-
-  return { 'r': r, 'g': g, 'b': b };
+  return {
+    r: parseInt(hex.slice(1, 3), 16),
+    g: parseInt(hex.slice(3, 5), 16),
+    b: parseInt(hex.slice(5, 7), 16)
+  }
 }
 
 function setValues() {
   inputs['scaleUp'].value = scaleUp;
   inputs['brightnessThreshold'].value = brightnessThreshold;
+  inputs['probability'].value = probability;
   inputs['pointSize'].value = pointSize;
   inputs['customFrameRate'].value = customFrameRate;
   inputs['mouseDistanceForInteraction'].value = mouseDistanceForInteraction;
+  inputs['pointColor'].value = pointColor;
 }
 
 function addListeners() {
   inputs['scaleUp'].addEventListener('input', event => scaleUp = event.target.value);
   inputs['brightnessThreshold'].addEventListener('input', event => brightnessThreshold = event.target.value);
+  inputs['probability'].addEventListener('input', event => probability = event.target.value);
   inputs['pointSize'].addEventListener('input', event => pointSize = event.target.value);
   inputs['customFrameRate'].addEventListener('input', event => customFrameRate = event.target.value);
   inputs['mouseDistanceForInteraction'].addEventListener('input', event => mouseDistanceForInteraction = event.target.value);
   inputs['pointColor'].addEventListener('input', event => pointColor = hexToRGB(event.target.value));
+  inputs['apply-changes'].addEventListener('click', event => {
+    setup();
+  });
 }
 
 function preload() {
   img = loadImage('bird-alt.png');
-  setValues();
   addListeners();
 }
 
 function setup() {
+  console.log(img);
+  setValues();
+  console.log({'scaleUp': scaleUp, 'brightnessThreshold': brightnessThreshold, 'pointSize': pointSize});
+  points = [];
   zeroVector = createVector(0, 0);
   scaleUp = Math.round((window.innerHeight / img.height) * 100) / 100;
   inverseScaleUp = Math.round((1 / scaleUp) * 100) / 100;
@@ -82,7 +94,7 @@ function setup() {
   for (let i = 0; i < img.pixels.length; i += 4) {
     // holds the average value from all of the channels (R, G, B) for the current pixel
     const avg = (img.pixels[i] + img.pixels[i+1] + img.pixels[i+2]) / 3;
-    const newCondition = Math.floor(Math.random() * 30);
+    const newCondition = Math.floor(Math.random() * probability);
 
     // clears out all the values of the current pixel
     img.pixels[i] = img.pixels[i + 1] = img.pixels[i + 2] = img.pixels[i + 3] = 0;
@@ -114,7 +126,7 @@ function setup() {
   }
 
   img.updatePixels();
-
+  console.log(points.length);
   scale(scaleUp);
 }
 
@@ -128,41 +140,44 @@ function draw() {
   scale(scaleUp);
 
   for (let i = 0; i < points.length; i++) {
-    points[i].behaviors();
-    points[i].draw();
-    points[i].update();
+    const point = points[i];
+
+    point.behaviors();
+    point.draw();
+    point.update();
   }
 
   fpsCounter.innerHTML = Math.round(frameRate());
-  frameRate(customFrameRate);
+  // frameRate(customFrameRate * 10);
 }
 
-function Point(x, y, opacity, dx, dy, orgX, orgY) {
-  // Point class definition
-  this.x = x;
-  this.y = y;
-  this.orgX = orgX;
-  this.orgY = orgY;
-  this.opacity = opacity;
-  this.dx = dx;
-  this.dy = dy;
+class Point {
+  constructor(x, y, opacity, dx, dy, orgX, orgY) {
+    this.x = x;
+    this.y = y;
+    this.orgX = orgX;
+    this.orgY = orgY;
+    this.opacity = opacity;
+    this.dx = dx;
+    this.dy = dy;
 
-  // VECTOR VARIABLES
-  this.pos = createVector(random(img.width), random(img.height));
-  this.vel = createVector(0);
-  this.acc = createVector(0);
-  this.target = createVector(x, y);
-  this.maxSpeed = (Math.random() * 2) + 0.2;
-  this.maxForce = 1;
-  // END-VECTOR VARIABLES
+    // VECTOR VARIABLES
+    this.pos = createVector(random(img.width), random(img.height));
+    this.vel = createVector(0);
+    this.acc = createVector(0);
+    this.target = createVector(x, y);
+    this.maxSpeed = (Math.random() * 40) + 10;
+    this.maxForce = 1;
+    // END-VECTOR VARIABLES
+  }
 
-  this.draw = function() {
+  draw() {
     fill(pointColor['r'], pointColor['g'], pointColor['b'], this.opacity);
     ellipse(this.pos.x, this.pos.y, pointSize, pointSize); // VECTOR DRAW
   }
 
   // VECTOR METHODS
-  this.update = function() {
+  update() {
     this.pos.add(this.vel);
     this.vel.add(this.acc);
     
@@ -170,9 +185,9 @@ function Point(x, y, opacity, dx, dy, orgX, orgY) {
     this.acc.mult(0);
   }
 
-  this.behaviors = function() {
-    let arrive = this.arrive(this.target);
-    let flee = this.flee(mouse);
+  behaviors() {
+    const arrive = this.arrive(this.target);
+    const flee = this.flee(mouse);
     
     arrive.mult(1);
     flee.mult(10);
@@ -181,15 +196,15 @@ function Point(x, y, opacity, dx, dy, orgX, orgY) {
     this.applyForce(flee);
   }
 
-  this.applyForce = function(force) {
+  applyForce(force) {
     this.acc.add(force);
   }
 
-  this.arrive = function(target) {
+  arrive(target) {
     // sub = subtract
-    let desiredLocation = p5.Vector.sub(target, this.pos);
-    let distance = desiredLocation.mag(); // magnitude means how far away the vector is
-    let speed = this.maxSpeed;
+    const desiredLocation = p5.Vector.sub(target, this.pos);
+    const distance = desiredLocation.mag(); // magnitude means how far away the vector is
+    let speed = this.maxSpeed * (distance / img.width);
 
     if (distance < 1) {
       speed = map(distance, 0, 1, 0, speed);
@@ -197,26 +212,26 @@ function Point(x, y, opacity, dx, dy, orgX, orgY) {
 
     desiredLocation.setMag(speed);
 
-    let steer = p5.Vector.sub(desiredLocation, this.vel);
+    const steer = p5.Vector.sub(desiredLocation, this.vel);
     steer.limit(this.maxForce);
     
     return steer;
   }
 
-  this.seek = function(target) {
+  seek(target) {
     // sub = subtract
-    let desiredLocation = p5.Vector.sub(target, this.pos);
+    const desiredLocation = p5.Vector.sub(target, this.pos);
     desiredLocation.setMag(speed);
 
-    let steer = p5.Vector.sub(desiredLocation, this.vel);
+    const steer = p5.Vector.sub(desiredLocation, this.vel);
     steer.limit(this.maxForce);
     
     return steer;
   }
 
-  this.flee = function(target) {
+  flee(target) {
     // sub = subtract
-    let desiredLocation = p5.Vector.sub(target, this.pos);
+    const desiredLocation = p5.Vector.sub(target, this.pos);
     const distance = desiredLocation.mag() + ((Math.random() * 10) - 10);
 
     // applies force only if mouse is close enough to the target
@@ -224,7 +239,7 @@ function Point(x, y, opacity, dx, dy, orgX, orgY) {
       desiredLocation.setMag(this.maxSpeed);
       desiredLocation.mult(-1);
 
-      let steer = p5.Vector.sub(desiredLocation, this.vel);
+      const steer = p5.Vector.sub(desiredLocation, this.vel);
       steer.limit(this.maxForce);
       
       return steer;
